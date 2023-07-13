bd39859e205ec534cd781c65a19013f24f23a7ec
Yehuda Podorovsky <podoro@gmail.com>
2023-07-13 12:19:48 +0300

    Max_palindrome

--- test.py
+++ test.py
@@ -1,4 +1,35 @@
-my_dict = {'apple': 5, 'banana': 6, 'orange': 6}
-key_list = list(my_dict.values())
+def diagonal_scan(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    output = []
 
-print(key_list)
+    # Scan upper diagonals
+    for c in range(cols):
+        diagonal = []
+        i = 0
+        j = c
+        while i < rows and j >= 0:
+            diagonal.append(matrix[i][j])
+            i += 1
+            j -= 1
+        output.append(diagonal)
+
+    # Scan lower diagonals
+    for r in range(1, rows):
+        diagonal = []
+        i = r
+        j = cols - 1
+        while i < rows and j >= 0:
+            diagonal.append(matrix[i][j])
+            i += 1
+            j -= 1
+        output.append(diagonal)
+
+    return output
+
+
+# Example input matrix
+input_matrix = [[9, 2, 3], [4, 5, 6], [7, 8, 1]]
+
+output = diagonal_scan(input_matrix)
+print(output)
