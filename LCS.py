da659a9c69002bd22d334c7f69209a185fddb579
Yehuda Podorovsky <podoro@gmail.com>
2023-07-13 09:30:41 +0300

    LCS

--- LCS.py
+++ LCS.py
@@ -1,29 +1,32 @@
+# LCS
 def longest_common_subsequence(text1, text2):
     m, n = len(text1), len(text2)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    OPT = [[0] * (n + 1) for _ in range(m + 1)]
 
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if text1[i - 1] == text2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+                OPT[i][j] = OPT[i - 1][j - 1] + 1
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-    for i in range(len(dp)):
-        print(dp[i])
+                OPT[i][j] = max(OPT[i - 1][j], OPT[i][j - 1])
+
+    print("OPT table")
+    for i in range(len(OPT)):
+        print(OPT[i])
     # Reconstruct the longest common subsequence
-    printLCSReco(dp, m, j)
     print()
+    printLCSReco(OPT, m, j)
+    print()
+    return printLCSiter(OPT, text1, text2, m, n)
 
-    return printLCSiter(dp, text1, text2, m, n)
-
-def printLCSiter(dp, text1, text2, i, j):
+def printLCSiter(OPT, text1, text2, i, j):
     lcs = []
     while i > 0 and j > 0:
         if text1[i - 1] == text2[j - 1]:
             lcs.insert(0, text1[i - 1])
             i -= 1
             j -= 1
-        elif dp[i - 1][j] > dp[i][j - 1]:
+        elif OPT[i - 1][j] > OPT[i][j - 1]:
             i -= 1
         else:
             j -= 1
@@ -40,10 +43,9 @@ def printLCSReco(b, i, j):
     else:
         printLCSReco(b, i, j-1)
 
-# Example usage:
 # text1 = "ACCGGTCGAGTGCGCGGAAGCCGGCCGAA"
 # text2 = "GTCGTTCGGAATGCCGTTGCTCTGTAAA"
 text1 = "ABCBDAB"
 text2 = "BDCABA"
-result = longest_common_subsequence(text1, text2)
-print(result)  # Output: ['A', 'D', 'H']
+print(longest_common_subsequence(text1, text2))
+
